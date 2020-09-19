a0c508583d19ba76d65882b27df75234a522b283
JohnYezub <buze@ya.ru>
2020-09-19 17:32:26 +0800

    Initial Commit

---
+++ VIPER demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER demo2
+//
+//  Created by   admin on 19.09.2020.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
